9edb13b15337611fa0f31abdb4c569ac9b753135
Suman Modak <sumanmodak578@gmail.com>
2024-12-29 00:03:32 +0530

    added templates and static html and css file for home page

--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Hello, world. You are at chai aur Django home page")
+    # return HttpResponse("Hello, world. You are at chai aur Django home page")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("Hello, world. You are at chai aur Django about page")
