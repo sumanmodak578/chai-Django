1967183b56fc40806ce87d205d60b05540e6dd64
Suman Modak <sumanmodak578@gmail.com>
2025-01-03 21:22:00 +0530

    install and configured tailwind in Django project

--- README.md
+++ README.md
@@ -13,4 +13,7 @@ command for locate ssh files
 
 ## Jinja2 and Django apps - (templates and its working)
 ### Read documentation of Django templates for further understanding from the below link
-[Django-Templetes]([Chai-Django](https://docs.chaicode.com/getting-started-with-django/))
+[Django-Templetes](https://docs.djangoproject.com/en/5.1/topics/templates//)
+
+## How to install tailwind in Django
+[click-here](https://docs.chaicode.com/tailwind-to-django/)
--- chaiaurDjango/chaiaurDjango/settings.py
+++ chaiaurDjango/chaiaurDjango/settings.py
@@ -39,7 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chai',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -49,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'chaiaurDjango.urls'
