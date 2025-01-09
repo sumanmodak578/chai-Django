c308f5eb1f21f8e92d3c5ccfbc2bb0e52cbfe423
Suman Modak <sumanmodak578@gmail.com>
2025-01-10 00:13:11 +0530

    django form integration in the app

---
+++ chaiaurDjango/chai/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import ChaiVarity
+
+
+
+class ChaiVarityForm(forms.Form):
+    chai_varity = forms.ModelChoiceField(queryset=ChaiVarity.objects.all(), label="Select Chai Varity")
